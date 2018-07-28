8dee9c3889bbd8f08a4647debeefa131559c24e9
soffarciniegas17 <sofiarciniegas17@gmail.com>
2018-07-28 08:37:49 -0500

    Initial commit

---
+++ app/src/main/java/com/sophia1/colorappnuevo/DataBase.java
@@ -0,0 +1,53 @@
+package com.sophia1.colorappnuevo;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DataBase extends SQLiteOpenHelper {
+
+    Context context;
+    private static final String dataName ="partida.db";
+    private static final String tableName = "CREARE TABLE JUEGO (ID INTEGER PRIMARY KEY AUTOINCREMENT, PUNTAJE DOUBLE)";
+    private static final int version = 1;
+
+    public DataBase(Context context){
+        super(context, dataName,null,version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(tableName);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS CREATE"+tableName);
+        db.execSQL(tableName);
+
+    }
+
+    public void guardar (double puntaje){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("PUNTAJE", puntaje);
+        db.insert("JUEGO", null, values);
+    }
+
+    public Cursor puntajes (){
+        SQLiteDatabase db = getReadableDatabase();
+        String find  [] = {"PUNTAJE"};
+        String orderBy = "PUNTAJE DESC";
+        String limit = "5";
+
+        Cursor cursor;
+        try {
+            cursor= db.query("JUEGO",find, null, null, null, orderBy, limit );
+            return cursor;
+        } catch (Exception e){}
+
+        return null;
+    }
+}
