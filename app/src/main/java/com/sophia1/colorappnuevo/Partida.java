3b4167df493e87e3e15e74f19d51381cde9686c0
soffarciniegas17 <sofiarciniegas17@gmail.com>
2018-07-28 08:44:08 -0500

    jeje

---
+++ app/src/main/java/com/sophia1/colorappnuevo/Partida.java
@@ -0,0 +1,13 @@
+package com.sophia1.colorappnuevo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Partida extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partida);
+    }
+}
